949c7aaed320f457b56c0f671ca2f09a52e9fae0
MissingXNO <62749630+MissingXNO@users.noreply.github.com>
2024-11-16 10:07:09 -0500

    Add files via upload
    Se corrigió la cantidad de especialidades y de doctores para coincidir con las solicitadas

--- medical-app/scripts/insert_data.sql
+++ medical-app/scripts/insert_data.sql
@@ -8,12 +8,16 @@ INSERT INTO patient (name, age, email, password) VALUES
 
 -- Insertar datos en la tabla Specialty
 INSERT INTO specialty (name) VALUES
-('Cardiology'), ('Neurology'), ('Orthopedics');
+('General'), ('Cardiology'), ('Urology'),
+('Pediatrics'), ('Physiology');
 
 -- Insertar datos en la tabla Doctor
 INSERT INTO doctor (name, age, email, password, specialty_id) VALUES
 ('Dr. Ana Perez', 45, 'ana.perez@example.com', 'hashedpassword6', 1),
-('Dr. Juan Martinez', 50, 'juan.martinez@example.com', 'hashedpassword7', 2);
+('Dr. Juan Martinez', 50, 'juan.martinez@example.com', 'hashedpassword7', 2),
+('Dr. Laura González', 38, 'laura.gonzalez@example.com', 'hashedpassword8', 3),
+('Dr. Pedro Sánchez', 42, 'pedro.sanchez@example.com', 'hashedpassword9', 4),
+('Dr. María Ruiz', 36, 'maria.ruiz@example.com', 'hashedpassword10', 5);
 
 -- Insertar datos en la tabla MedicalAppointment
 INSERT INTO medicalappointment (date, hour, patient_id, doctor_id) VALUES
